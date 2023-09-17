5bbf5569c708f2e96a658648670cb962ad923e8d
VX10 <git3git@gmail.com>
2023-09-17 13:15:17 +0400

    class field_cell romb2

--- field_cell.py
+++ field_cell.py
@@ -14,27 +14,44 @@ class FieldCell():
     # 1 - rope with a bell     / веревочка с колокольчиком
     # 2 - protoplasm detector  / детектор протоплазмы
     cell_type = None
+    fill_color = None
 
     def __init__(self, cell_type):
         self.cell_type = cell_type
+        self.fill_color = QColor(255, 255, 255)
 
     def draw_rect(self, painter):
         pass
 
         size = 100
         painter.setPen(QPen(QColor(0, 0, 0), 5))
-        painter.setBrush(QColor(255, 0, 0))
+        painter.setBrush(self.fill_color)
         painter.drawRect(self.cell_coordinates[0], self.cell_coordinates[1], size, size)
 
-        if self.cell_type == 0 or self.cell_type == 1 or self.cell_type == 2:
+        # ромб (веревочка с колокольчиком)
+        if self.cell_type == 1:
             points = QPolygon([
                 QPoint(self.cell_coordinates[0] + 50, self.cell_coordinates[1] + 20),   # Вершина A
                 QPoint(self.cell_coordinates[0] + 90, self.cell_coordinates[1] + 50),   # Вершина B
                 QPoint(self.cell_coordinates[0] + 50, self.cell_coordinates[1] + 80),   # Вершина C
                 QPoint(self.cell_coordinates[0] + 10, self.cell_coordinates[1] + 50)    # Вершина D
             ])
-            # painter.setPen(Qt.black)  # Устанавливаем цвет линии
             painter.setPen(QPen(Qt.black, 5))
-            painter.setBrush(QColor(255, 0, 0))  # Устанавливаем цвет заливки (красный)
-            painter.drawPolygon(points)  # Рисуем ромб
-            pass
+            # painter.setBrush(QColor(255, 0, 0))
+            painter.drawPolygon(points)
+
+        # звезда (детектор протоплазмы)
+        if self.cell_type == 2:
+            points = QPolygon([
+                QPoint(self.cell_coordinates[0] + 50, self.cell_coordinates[1] + 10),
+                QPoint(self.cell_coordinates[0] + 60, self.cell_coordinates[1] + 40),
+                QPoint(self.cell_coordinates[0] + 90, self.cell_coordinates[1] + 50),
+                QPoint(self.cell_coordinates[0] + 60, self.cell_coordinates[1] + 60),
+                QPoint(self.cell_coordinates[0] + 50, self.cell_coordinates[1] + 90),
+                QPoint(self.cell_coordinates[0] + 40, self.cell_coordinates[1] + 60),
+                QPoint(self.cell_coordinates[0] + 10, self.cell_coordinates[1] + 50),
+                QPoint(self.cell_coordinates[0] + 40, self.cell_coordinates[1] + 40)
+            ])
+            painter.setPen(QPen(Qt.black, 5))
+            # painter.setBrush(QColor(255, 0, 0))
+            painter.drawPolygon(points)
